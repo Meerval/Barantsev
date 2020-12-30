4d5b3185437fa407cb2d4c175cf9dc35ccd9af85
Meerval <gweerval@gmail.com>
2020-12-30 10:57:40 +0300

    8.7 done, James doesn't works and that is OK

--- src/test/java/ru/stqa/pft/mantis/tests/RegistrationTests.java
+++ src/test/java/ru/stqa/pft/mantis/tests/RegistrationTests.java
@@ -25,8 +25,10 @@ public class RegistrationTests extends TestBase {
         String email = String.format("email%s@users7.com", now);
         String user = String.format("user7%s", now);
         String password = "password";
+//        app.james().createUser(user,password);
         app.registration().start(user, email);
         List<MailMessage> mailMessages = app.mail().waitForMail(2, 10000);
+//        List<MailMessage> mailMessages = app.james().waitForMail(user,password, 60000);
         String confirmationLink = findConfirmationLink(mailMessages, email);
         app.registration().finish(confirmationLink, password, user);
         assertTrue(app.newSession().login(user, password));
