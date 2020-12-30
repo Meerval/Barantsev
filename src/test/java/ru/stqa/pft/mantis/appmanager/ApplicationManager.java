52c34965fb6bc732aec3c44ade929dfa8bd4aff9
Meerval <gweerval@gmail.com>
2020-12-30 14:41:58 +0300

    9.3 done, soap helper has been created

--- src/test/java/ru/stqa/pft/mantis/appmanager/ApplicationManager.java
+++ src/test/java/ru/stqa/pft/mantis/appmanager/ApplicationManager.java
@@ -19,6 +19,7 @@ public class ApplicationManager {
     private FtpHelper ftp;
     private MailHelper mailHelper;
     private JamesHelper jamesHelper;
+    private SoapHelper soapHelper;
 
     public ApplicationManager(String browser) {
         this.browser = browser;
@@ -84,5 +85,12 @@ public class ApplicationManager {
             jamesHelper = new JamesHelper(this);
         }
         return jamesHelper;
+    }
+
+    public SoapHelper soap() {
+        if (soapHelper == null) {
+            soapHelper = new SoapHelper(this);
+        }
+        return soapHelper;
     }
 }
